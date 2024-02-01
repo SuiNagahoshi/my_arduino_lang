e67915b85a461d7337a387493e9fdb7337631acd
Sui Nagahoshi <119501407+SuiNagahoshi@users.noreply.github.com>
2024-02-01 13:48:45 +0900

    coding read file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::io::Read;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    println!("{:?}", args);
+    //println!("{:?}", args);
 
     let source_path = &args[1];
 
     println!("{:?}", source_path);
+
+    let mut source = std::fs::File::open(source_path).expect("File not found.");
+    let mut source_contents = String::new();
+
+    source.read_to_string(&mut source_contents).expect("Something went wrong reading the file.");
+
+    println!("The texts:\n{}", source_contents);
 }
